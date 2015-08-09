7ad5d9ec6fd735aade481700e5274ff63a785a93
Tamas Rev <extern.trev@porsche.hu>
2015-08-09 18:16:10 +0200

    adding number test for iterable

---
+++ src/test/java/com/trev/fizzbuzz/inject/tapestry/services/TestFizzBuzzNumbers.java
@@ -0,0 +1,67 @@
+package com.trev.fizzbuzz.inject.tapestry.services;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Iterator;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestFizzBuzzNumbers
+{
+
+    private Iterator<Integer> nums;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        final FizzBuzzNumberProvider sut = new DefaultNumberProvider();
+        final Iterable<Integer> stream = sut.getFizzBuzzNumbers();
+        nums = stream.iterator();
+    }
+
+    @Test
+    public void testFizzBuzzNumbersStart()
+    {
+        // THEN
+        assertEquals(nums.next(), new Integer(1));
+    }
+
+    @Test
+    public void testFizzBuzzNumbersEnd()
+    {
+        // WHEN
+        Integer i = null;
+        while (nums.hasNext())
+        {
+            i = nums.next(); // i is last from iterator now
+        }
+
+        // THEN
+        assertEquals(i, new Integer(100));
+    }
+
+    @Test
+    public void testFizzBuzzNumbersStep()
+    {
+        // GIVEN
+        Integer previous = null;
+        boolean first = true;
+
+        // WHEN
+        while (nums.hasNext())
+        {
+            final Integer actual = nums.next();
+            if (!first)
+            {
+                // THEN
+                assertEquals(actual - previous, 1);
+            }
+            else
+            {
+                first = false;
+            }
+            previous = actual;
+        }
+    }
+}
